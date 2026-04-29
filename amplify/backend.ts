da600804e6cba957993601ff4316543d842272e1
helmysaeed <helmy.saeed@gmail.com>
2026-04-29 18:25:09 +0300

    Update backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,7 +1,7 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
-
+import { myFirstFunction } from './my-first-function/resource';
 defineBackend({
   auth,
   data,
